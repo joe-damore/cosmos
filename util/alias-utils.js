a94346aface56ededf3faa96d6b6ebe9df570cb1
Joe D'Amore <joe@joedamore.me>
2021-02-14 17:45:16 -0500

    Add util functions for compilation

---
+++ util/alias-utils.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+/**
+ * Returns a map of compilation path aliases.
+ *
+ * If build is for a renderer, as indicated by the supplied options, then
+ * additional aliases will be included.
+ *
+ * @param {string} rootPath - Root project path.
+ * @param {Object=} options - Options.
+ * @param {boolean=} options.renderer - Whether build is for a renderer.
+ * @param {string=} options.rendererName - Renderer name.
+ *
+ * @return {Object} Path aliases.
+ */
+const getAliases = (rootPath, options = {}) => {
+  const rendererOnlyOptions = (() => {
+    if (options.renderer && options.rendererName) {
+      return {
+        '@renderer': path.resolve(rootPath, 'src', 'renderers', options.rendererName),
+      };
+    }
+    return [];
+  });
+
+  return {
+    '@core': path.resolve(rootPath, 'src', 'core'),
+    '@main': path.resolve(rootPath, 'src', 'main'),
+    '@app': path.resolve(rootPath, 'src', 'main', 'app'),
+    '@renderers': path.resolve(rootPath, 'src', 'renderers'),
+    ...rendererOnlyOptions,
+  };
+};
+
+module.exports = {
+  getAliases,
+};
---
+++ util/renderer-utils.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+// Name of file that must exist for a renderer to be recognized.
+const ENTRY_FILE = 'index.ts';
+
+/**
+ * Returns an array of objects describing the name and entrypoint of a renderer.
+ *
+ * Each object contains a `name` and `entrypoint` property.
+ *
+ * Requires Node v10+.
+ *
+ * @param {string} rendererPath - Absolute path to renderer directory.
+ *
+ * @returns {Object[]} Array of renderer objects.
+ */
+const getRenderers = (rendererPath) => {
+  const absolutePath = path.resolve(rendererPath);
+  return fs.readdirSync(absolutePath, { withFileTypes: true })
+    .filter((dirent) => { return dirent.isDirectory(); })
+    .filter((dirent) => { return fs.existsSync(path.join(absolutePath, dirent.name, ENTRY_FILE)); })
+    .map((dirent) => {
+      return {
+        name: dirent.name,
+        entrypoint: path.join(absolutePath, dirent.name, ENTRY_FILE),
+      };
+    });
+};
+
+module.exports = {
+  getRenderers,
+};
