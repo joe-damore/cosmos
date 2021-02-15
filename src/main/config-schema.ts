917b84a1000d10415a1e923ed804acfad696bf48
Joe D'Amore <joe@joedamore.me>
2021-02-15 15:58:40 -0500

    Add db configuration to schema

--- src/main/config-schema.ts
+++ src/main/config-schema.ts
@@ -1,39 +1,17 @@
-//const configSchema = {
-//  library: {
-//    doc: 'Media library configuration',
-//    format: 'source-array',
-//    default: [],
-//    children: {
-//      doc: 'A collection of media sources',
-//      format: 'object',
-//      paths: {
-//        media: {
-//          doc: 'Path to media files',
-//          format: 'string',
-//        },
-//        metadata: {
-//          doc: 'Path to metadata files',
-//          format: 'string',
-//        },
-//        art: {
-//          doc: 'Path to art files',
-//          format: 'string',
-//        },
-//        saves: {
-//          doc: 'Path to save files',
-//          format: 'string',
-//        },
-//      },
-//    },
-//  },
-//};
 const configSchema = {
   type: 'object',
   additionalProperties: false,
   properties: {
-    a: {
-      type: 'string',
-      description: 'The letter A',
+    db: {
+      type: 'object',
+      description: 'Configuration details for Cosmos database',
+      additionalProperties: false,
+      properties: {
+        path: {
+          type: 'string',
+          description: 'Path to Cosmos database file',
+        },
+      },
     },
   },
 };
